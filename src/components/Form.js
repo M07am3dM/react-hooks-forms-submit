40a5d542ec0ddb10c7d13ac60b894d1d6086c774
mohamed <matassi.mohamed89@gmail.com>
2023-09-12 10:54:32 +0100

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 
 function Form(props) {
-  const [firstName, setFirstName] = useState("Sylvia");
-  const [lastName, setLastName] = useState("Woods");
+  const [firstName, setFirstName] = useState('Mohamed');
+  const [lastName, setLastName] = useState('Matassi');
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (firstName.length > 0) {
+      const formData = {
+        firstName: firstName,
+        lastName: lastName,
+      };
+      const dataArray = [...submittedData, formData];
+      // props.sendFormDataSomewhere(formData);
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, i) => {
+    return (
+      <div key={i}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -13,11 +40,22 @@ function Form(props) {
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {errors.length > 0
+        ? errors.map((error, i) => (
+            <p key={i} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
   );
 }
 
